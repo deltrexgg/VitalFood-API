56f5d0d4ed5ea1efe38b0ee2c869f9f16019075a
deltrex <deltrexgg@gmail.com>
2024-02-10 14:18:58 +0530

    about title

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 app.set('view engine', 'ejs')
 
+
 // Define a Mongoose schema
 const userSchema = new mongoose.Schema({
   condition: String,
